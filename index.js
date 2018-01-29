9f487303fbb8a241f4a7d4ae1c67d4acfaf8896e
Markus <markus.olkinuora@oddy.fi>
2018-01-29 08:45:29 +0200

    -

--- index.js
+++ index.js
@@ -10,23 +10,25 @@
             Vue.mixin({
                 data() {
                     var vm = this, data = {}, lsd = this.$options.localStorageData
-                    if ( typeof lsd === 'function'  ) lsd = lsd(); else return data
                     if ( typeof lsd === 'undefined' ) return data
-                    Object.keys(lsd).forEach(function (k) {
-                        if ((' lsd_id lsd_throttle ').indexOf(' ' + k + ' ') > -1) return
-                        var lk = opts.prefix + (lsd.lsd_id || vm._uid) + '_' + k, lv = localStorage.getItem(lk)
-                        if (lv === null) localStorage.setItem(lk, JSON.stringify(data[k] = lsd[k]))
-                        else data[k] = JSON.parse(lv)
-                        vm.$nextTick(function () {
-                            this.$watch(k, function (n) {
-                                if ( timeouts[lk] ) return
-                                timeouts[lk] = setTimeout(function () {
-                                    timeouts[lk] = false
-                                    localStorage.setItem(lk, JSON.stringify(vm[k]))
-                                }, lsd.lsd_throttle || opts.throttle)
-                            }, { deep: true })
-                        });
-                    })
+                    if ( typeof lsd === 'function'  ) lsd = lsd();
+                    if ( lsd !== null && typeof lsd == 'object' ) {
+                        Object.keys(lsd).forEach(function (k) {
+                            if ((' lsd_id lsd_throttle ').indexOf(' ' + k + ' ') > -1) return
+                            var lk = opts.prefix + (lsd.lsd_id || vm._uid) + '_' + k, lv = localStorage.getItem(lk)
+                            if (lv === null) localStorage.setItem(lk, JSON.stringify(data[k] = lsd[k]))
+                            else data[k] = JSON.parse(lv)
+                            vm.$nextTick(function () {
+                                this.$watch(k, function (n) {
+                                    if ( timeouts[lk] ) return
+                                    timeouts[lk] = setTimeout(function () {
+                                        timeouts[lk] = false
+                                        localStorage.setItem(lk, JSON.stringify(vm[k]))
+                                    }, lsd.lsd_throttle || opts.throttle)
+                                }, { deep: true })
+                            });
+                        })
+                    } else return lsd
                     return data
                 }
             })
