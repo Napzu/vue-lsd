8584bd5e0dfe1180efdfa2a7f56c7577636cc3de
Markus <markus.olkinuora@oddy.fi>
2018-02-07 16:08:38 +0200

    -

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
-    ( global.vueLocalStorageData = factory() );
+    (global.vueLocalStorageData = factory());
 }(this, (function () { 'use strict';
     var LSD = {
-        install: function(Vue, o) {
-            var timeouts = {}, opts = { prefix: 'vueLocalStorageData_', throttle: 500 }, isNode = new Function("try{return this===global}catch(e){return false}")()
+        install: function (Vue, o) {
+            var timeouts = {}, opts = { prefix: 'vueLocalStorageData_', throttle: 500 }, isNode = new Function("return this && !this.localStorage")()
             if (o !== null && typeof o === 'object') Object.keys(o).forEach(function (k) { opts[k] = o[k] })
             Vue.mixin({
-                data: function() {
+                data: function () {
                     var vm = this, data = {}, lsd = this.$options.localStorageData;
-                    if ( typeof lsd === 'undefined' ) return data
-                    if ( typeof lsd === 'function' ) lsd = lsd();
-                    if ( isNode ) return lsd
-                    if ( lsd !== null && typeof lsd == 'object' ) {
+                    if (typeof lsd === 'undefined') return data
+                    if (typeof lsd === 'function') lsd = lsd.call(vm);
+                    if (isNode) return lsd
+                    if (lsd !== null && typeof lsd == 'object') {
                         Object.keys(lsd).forEach(function (k) {
                             if ((' lsd_id lsd_throttle ').indexOf(' ' + k + ' ') > -1) return
                             var lk = opts.prefix + (lsd.lsd_id || vm._uid) + '_' + k, lv = localStorage.getItem(lk)
@@ -21,7 +21,7 @@
                             else data[k] = JSON.parse(lv)
                             vm.$nextTick(function () {
                                 this.$watch(k, function (n) {
-                                    if ( timeouts[lk] ) return
+                                    if (timeouts[lk]) return
                                     timeouts[lk] = setTimeout(function () {
                                         timeouts[lk] = false
                                         localStorage.setItem(lk, JSON.stringify(vm[k]))
